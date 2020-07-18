6eba74aec62b0ca172243326b679683ab976cc82
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-07-18 20:12:59 +0100

    Added testing functionality for very large numbers and decimal numbers.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -58,5 +58,24 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-1')
   })
 
+  it('should output a number as a decimal', function(){
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.75')
+  })
+
+  it('should output a very large number', function(){
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6561')
+  })
 
 });
